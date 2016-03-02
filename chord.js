5f616cd5d96983d87d5e7eff3467baa4f6f6d656
Judy Chang <judychang@Judys-MacBook-Pro-2.local>
2016-03-02 16:26:49 -0500

    ugly arrowheads

--- chord.js
+++ chord.js
@@ -3,21 +3,21 @@
 /// matrix[i][j] is number of attacks candidate i has on candidate j
 var matrix = [
   [0, 0, 0, 0, 0, 0], // DONALD TRUMP 0
-  [20, 0, 500, 100, 120, 10], // JEB BUSH 1
+  [20, 0, 100, 20, 5, 10], // JEB BUSH 1
   [30, 20, 0, 30, 40, 50], // MARCO RUBIO 2
   [10, 20, 10, 0, 0, 0], // TED CRUZ 3
   [20, 20, 100, 40, 0, 50], // BEN CARSON 4
-  [10, 20, 30, 0, 500, 0] // JOHN KASICH 5
+  [10, 20, 30, 0, 100, 0] // JOHN KASICH 5
 ];
 // TODO: PUT IN REAL DATA
 
 var candidatesList = ["Donald Trump", "Jeb Bush", "Marco Rubio", "Ted Cruz", "Ben Carson", "John Kasich"];
-var maxValue = 500;
+var maxValue = 100;
 
 // SVG DIMENSIONS
 var width = 960;
-var height = 500;
-var radius = Math.min(width, height) * .41;
+var height = 800;
+var radius = Math.min(width, height) * 0.5;
 var avatarRadius = 30;
 
 // CREATE SVG OBJECT
@@ -54,20 +54,16 @@ var angle = d3.scale.linear()
 // APPEND ARROWS
 // TODO: ARROWHEAD
 
-svg.append("svg:defs").append("svg:marker")
-  .attr("id", "arrowhead")
-  .attr("viewBox", "0 0 4 4")
-  .attr("refX", 4)
-  .attr("refY", 2)
-  .attr("orient", "auto")
-  .append("svg:path")
-  .attr("d", "M 1.5 0 L 4 2 L 1.5 4 z");
-
-var arrows = svg.append("g").attr("class", "arrows")
-  .selectAll("path")
+var arrowGroup = svg
+  .selectAll("g.arrows")
   .data(chord.chords)
   .enter()
-  .append("path")
+  .append("g").attr("class", "arrows")
+
+arrowGroup.append("path")
+  .style("stroke-width", function(d) { return lineWidth(d.source.value); })
+  .style("stroke", function(d) { return colors(d.source.index); })
+  .style("opacity", 0.5)
   .attr("d", function(d) {
     var x1 = radius * Math.cos(angle(d.source.index));
     var y1 = radius * Math.sin(angle(d.source.index));
@@ -79,12 +75,9 @@ var arrows = svg.append("g").attr("class", "arrows")
     var len = Math.sqrt(dx * dx + dy * dy);
     var offsetX = dx * avatarRadius / len;
     var offsetY = dy * avatarRadius / len;
-    return "M " + x1 + " " + y1 + " L " + (x2 - offsetX) + " " + (y2 - offsetY);
+    return "M " + x1 + " " + y1 + " L " + (x2 - offsetX ) + " " + (y2 - offsetY);
   })
-  .attr("marker-end", "url(\#arrowhead)")
-  .style("stroke", function(d) { return colors(d.source.index); })
-  .style("stroke-width", function(d) { return lineWidth(d.source.value); })
-  .style("opacity", 0.5);
+  .attr("marker-end", function(d) { return getArrowhead(d.source.index); });
 
 // TODO: ADD AVATARS?
 svg.append("g").attr("class", "avatars")
@@ -101,9 +94,33 @@ svg.append("g").attr("class", "avatars")
 
 
 // fade all lines except for self
+
+function getArrowhead(i) {
+  var marker = svg.append("svg:defs").append("svg:marker")
+    .attr("id", "arrowhead" + i)
+    .attr("markerWidth", 4)
+    .attr("markerHeight", 4)
+    .attr("refX", 2)
+    .attr("refY", 2)
+    .attr("orient", "auto");
+
+  marker.append("svg:path")
+    .attr("d", "M 2 2 L 1 1 L 1 3 z")
+    .style("fill", function(d) { return colors(i); });
+
+  marker.append("svg:path")
+    .attr("d", "M 2 2 L 2 3 L 1 3 z")
+    .style("fill", "white");
+  marker.append("svg:path")
+    .attr("d", "M 2 2 L 2 1 L 1 1 z")
+    .style("fill", "white");
+
+  return "url(\#arrowhead" + i + ")"
+}
+
 function fade(opacity, source) {
   return function(g, i) {
-    svg.selectAll(".arrows line")
+    svg.selectAll(".arrows path")
         .filter(function(d) {
           return d.source.index != i;
         })
