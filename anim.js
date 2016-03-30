fcf84fe1c03abfbf35f272043797f518c769d9f1
Judy Chang <judychang@dhcp-18-189-39-29.dyn.mit.edu>
2016-03-30 16:50:12 -0400

    switched to proejct 3

---
+++ anim.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	var items = ["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8"];
+	var delays = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000];
+
+	for (var i = 0; i < items.length; i++) {
+		$("#" + items[i]).delay(delays[i]).fadeIn(400);
+	}
+})
