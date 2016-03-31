b4af60fb2bd58e6ac5dbfa2c6d1c297e6d5d0d45
Eric Lau <eric_l@mit.edu>
2016-03-30 22:13:50 -0400

    Update

--- anim.js
+++ anim.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var items = ["s_1", "s_2", "s_3", "s_4", "s_5", "s_6", "s_7", "s_8"];
-	var delays = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000];
+	var delays = [115000, 116000, 124000, 132000, 136000, 140000, 145000, 150000];
 
 	for (var i = 0; i < items.length; i++) {
 		$("#" + items[i]).delay(delays[i]).fadeIn(600);
